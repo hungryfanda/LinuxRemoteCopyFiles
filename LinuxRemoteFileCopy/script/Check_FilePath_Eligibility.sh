2aa006d63b716430c3567db5802d997d65e9696d
HungryFanda <cryptohelium@gmail.com>
2025-02-16 13:29:23 +0530

    LRFC-1004: FilePath Eligibility Check Added

--- LinuxRemoteFileCopy/script/Check_FilePath_Eligibility.sh
+++ LinuxRemoteFileCopy/script/Check_FilePath_Eligibility.sh
@@ -10,12 +10,67 @@ filepath="$1"
 #Excluded Paths
 excluded_paths=("/" "/bin" "/usr" "/opt" "/root") #Paths where Files are not Eligible to be Copied
 
+#Flag Variables
+excludedpath_flag="1"
+filepath_availability_flag="0"
+owner_flag="0"
+group_flag="0"
+
+#Check For Excluded Paths
 if [[ " ${excluded_paths[*]} " =~ " ${filepath} " ]]; then
     # whatever you want to do when array contains value
     echo "ExcludedPath"
-fi
-
-if [[ ! " ${excluded_paths[*]} " =~ " ${filepath} " ]]; then
+elif [[ ! " ${excluded_paths[*]} " =~ " ${filepath} " ]]; then
     # whatever you want to do when array doesn't contain value
     echo "Not ExcludedPath"
+    excludedpath_flag="0"
+fi
+
+if [[ "$excludedpath_flag" == "0" ]]; then
+    #Check if File Exists
+    filepath_check=$(ls -d "$filepath" 2>&1)
+    filepath_check=$(echo "$filepath_check" | awk '{$1=$1;print}')
+
+    if [[ "$filepath_check" == *"No such file or directory"* ]]; then
+        echo "Filepath $filepath does not Exist!"
+    else
+        echo "Filepath $filepath Available!"
+        filepath_availability_flag="1"
+    fi
+fi
+
+#Following Checks only apply to Destination Server
+if [[ "$server_type" == "DEST" ]]; then
+    if [[ "$filepath_availability_flag" == "1" ]]; then
+        #Check if owner Exists on the Server
+        owner_check=$(id -un "$owner" 2>&1)
+        owner_check=$(echo "$owner_check" | awk '{$1=$1;print}')
+        if [[ "$owner_check" == "$owner" ]]; then
+           echo "$owner Account Exists!"
+           owner_flag="1"
+        else
+            echo "$owner Account does not Exist!"
+        fi
+    fi
+
+    if [[ "$owner_flag" == "1" ]]; then
+        #Check if $owner has $group as Primary Group
+        group_check=$(id -gn "$owner" | grep -o "$group" 2>&1)
+        group_check=$(echo "$group_check" | awk '{$1=$1;print}')
+        if [[ "$group_check" == "$group" ]]; then
+            echo "$group Primary Group Available"
+            group_flag="1"
+        else
+            echo "$group Primary Group NOT Available"
+            #Check if $owner has $group as Secondary Group
+            group_check=$(id -Gn "$owner" | grep -o "$group" 2>&1)
+            group_check=$(echo "$group_check" | awk '{$1=$1;print}')
+            if [[ "$group_check" == "$group" ]]; then
+                echo "$group Secondary Group Available"
+                group_flag="1"
+            else
+                echo "$group Secondary Group NOT Available"
+            fi
+        fi
+    fi
 fi
--- LinuxRemoteFileCopy/script/test.sh
+++ LinuxRemoteFileCopy/script/test.sh
@@ -5,6 +5,8 @@ echo "---- LinuxRemoteCopy Script ---"
 #Common Variables
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "SCRIPT_DIR - $SCRIPT_DIR"
+ansible_dir="$SCRIPT_DIR/../playbook"
+echo "Ansible Directory - $ansible_dir"
 pwd=$(pwd)
 echo "PWD - $pwd"
 
@@ -26,11 +28,17 @@ declare -a dest_permission_array
 copy_details="$SCRIPT_DIR/properties/copy_details.txt" #Get Source & Destination details for Copy Files
 while read line
 do
-	line_arr=($(echo "$line" | tr ',' ' '))
+	#line_arr=($(echo "$line" | tr ',' ' '))
 	read -r src_server src_filepath dest_server dest_filepath dest_owner dest_group dest_permissions <<< $(echo "$line" | tr ',' ' ')
-	if [ "$src_server" -eq "$dest_server" ];then
-		echo "Same Server File Copy"
-	elif [[ "$src_server" -ne "$dest_server" ]]; then
-		echo "Different Server File Copy"
+	echo "($src_server to $dest_server)"
+	if [[ "$src_server" == "$dest_server" ]];then
+		echo "Same Server File Copy ($src_server to $dest_server)"
+	elif [[ "$src_server" != "$dest_server" ]]; then
+		echo "Different Server File Copy  ($src_server to $dest_server)"
 	fi
 done < "$copy_details"
+
+for file in $ansible_dir
+do
+	echo "$file"
+done
